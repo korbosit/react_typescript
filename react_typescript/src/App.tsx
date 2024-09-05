6663be7c277be1a3c5ebf42ffa78af1640d978dc
Vlaimir Korba <krissky1986@gmail.com>
2024-09-05 23:39:42 +0300

    #6.1_Event Props Example 1

--- react_typescript/src/App.tsx
+++ react_typescript/src/App.tsx
@@ -1,18 +1,14 @@
 import "./App.css";
-import Greet from "./components/Greet";
-import Heading from "./components/Heading";
-import Oscar from "./components/Oscar";
-import Status from "./components/Status";
+import Button from "./components/Button";
 
 function App() {
     return (
         <div>
-            <Status status="loading" />
-            <Heading>Placeholder text</Heading>
-            <Oscar>
-                <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
-            </Oscar>
-            <Greet name="Volodia" isLoggedIn={false} />
+            <Button
+                handleClick={(event, id) => {
+                    console.log("Button clicked", event, id);
+                }}
+            />
         </div>
     );
 }
---
+++ react_typescript/src/components/Button.tsx
@@ -0,0 +1,15 @@
+// type ButtonProps = {
+//     handleClick: () => void;
+// };
+type ButtonProps = {
+    handleClick: (
+        event: React.MouseEvent<HTMLButtonElement>,
+        id: number
+    ) => void;
+};
+
+export default function Button(props: ButtonProps) {
+    return (
+        <button onClick={(event) => props.handleClick(event, 1)}>Click</button>
+    );
+}
