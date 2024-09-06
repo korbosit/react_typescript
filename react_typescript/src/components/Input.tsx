3913bbe0a3655e039a5bd3f391143b82a4c06c47
Vlaimir Korba <krissky1986@gmail.com>
2024-09-06 10:18:17 +0300

    #8_Prop_Types_and_Tips

--- react_typescript/src/components/Input.tsx
+++ react_typescript/src/components/Input.tsx
@@ -3,11 +3,9 @@ type InputProps = {
     handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-export default function Input(props: InputProps) {
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(event);
-    };
-    return (
-        <input type="text" value={props.value} onChange={handleInputChange} />
-    );
+export default function Input({ value, handleChange }: InputProps) {
+    // const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     console.log(event);
+    // };
+    return <input type="text" value={value} onChange={handleChange} />;
 }
