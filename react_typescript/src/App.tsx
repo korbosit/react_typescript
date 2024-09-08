df6763e9a936d072f3e497458519a0e66ae9d0df
Vlaimir Korba <krissky1986@gmail.com>
2024-09-08 15:59:49 +0300

    #20_Restricting_Props

--- react_typescript/src/App.tsx
+++ react_typescript/src/App.tsx
@@ -1,34 +1,10 @@
 import "./App.css";
-import { List } from "./components/generics/List";
+import RandomNumber from "./components/restriction/RandomNumber";
 
 function App() {
     return (
         <div className="App">
-            {/* <List
-                items={["Vova", "Sveto444ka", "Alfochka"]}
-                onClick={(item) => console.log(item)}
-            />
-            <List items={[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
-            <List
-                items={[
-                    {
-                        id: 1,
-                        first: "Bruce ",
-                        last: "Lee",
-                    },
-                    {
-                        id: 2,
-                        first: "Bruce ",
-                        last: "Willis",
-                    },
-                    {
-                        id: 3,
-                        first: "Michael ",
-                        last: "Jordan",
-                    },
-                ]}
-                onClick={(item) => console.log(item)}
-            />
+            <RandomNumber value={10} isPositive />
         </div>
     );
 }
