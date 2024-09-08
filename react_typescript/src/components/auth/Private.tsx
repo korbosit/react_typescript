78fe1984cafd229a89dbf8aa9d32910b3f81c2fc
Vlaimir Korba <krissky1986@gmail.com>
2024-09-08 15:03:28 +0300

    #18_Component_Prop

---
+++ react_typescript/src/components/auth/Private.tsx
@@ -0,0 +1,16 @@
+import Login from "./Login";
+import { ProfileProps } from "./Profile";
+
+type PrivateProps = {
+    isLoggedIn: boolean;
+    component: React.ComponentType<ProfileProps>;
+};
+
+export const Private = ({ isLoggedIn, component: Component }: PrivateProps) => {
+    if (isLoggedIn) {
+        return <Component name="vovo444ka" />;
+    } else {
+        return <Login />;
+    }
+    return <div></div>;
+};
